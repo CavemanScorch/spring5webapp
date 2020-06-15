e6d4e3d175f0df2d1a62508c99059e92be33176b
Dustin Wright <nzeroxpd@gmail.com>
2020-06-15 18:16:56 -0500

    Added annotations to the Author and Book classes setting them up as JPA entities.

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,12 +1,22 @@
 package guru.springframework.spring5webapp.domain;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
 /**
  * Created by DW 06/15/20
  */
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String title;
     private String isbn;
     private Set<Author> authors;
@@ -18,6 +28,14 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
