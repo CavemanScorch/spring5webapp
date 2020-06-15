65786dfc16eecfe621737161718cf829519ab546
Dustin Wright <nzeroxpd@gmail.com>
2020-06-15 17:53:01 -0500

    Added fields to the Author class.

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,4 +1,14 @@
 package guru.springframework.spring5webapp.domain;
 
+
+import java.util.Set;
+
+/**
+ * Created by DW 06/15/20
+ */
+
 public class Author {
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
 }
